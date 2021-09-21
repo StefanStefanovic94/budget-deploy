5548e2eb17eb6260f3f73324ddebb164cb03c373
stefanstefanovic94 <stefan.stefanovic@kortechs.io>
2021-09-21 15:41:29 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Login from "./components/login/Login"
+import { Switch, Link, Route } from "react-router-dom"
+import Wallet from "./components/wallet/Wallet"
+import "./app.css"
+import AddExpenses from "./components/addExpenses/addExpenses"
+import AddIncome from "./components/addIncome/AddIncome"
+import LogOut from "./components/logout/Logout"
+import Register from "./components/register/Register"
+
+function App() {
+  return (
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Login}></Route>
+        <Route exact path="/wallet" component={Wallet}></Route>
+        <Route exact path="/wallet/addexpenses" component={AddExpenses}></Route>
+        <Route exact path="/wallet/addincome" component={AddIncome}></Route>
+        <Route exact path="/logout" component={LogOut}></Route>
+        <Route exact path="/register" component={Register}></Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
